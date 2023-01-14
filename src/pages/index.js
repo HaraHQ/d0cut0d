b247fa0a5745f7d2e9c25399c8e3ec31358a47c1
Rizkiansyah <hara.bekerja@gmail.com>
2023-01-14 15:37:05 +0700

    fix meta property

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ export const Head = () => {
   return (
     <>
       <title>Docutod</title>
-      <meta title="og:title" content="Docutod" />
-      <meta title="og:image" content="https://lh4.googleusercontent.com/qMhAhZstz_q-VSiKxVJmolEpJ4hiG_Nqhx7FlPBlWq2CF6YDLQMx_VLT7iLhjnOtsn50IQcRsO-Y0uc9nXgychOqAlkh7THK4wWNHXqM8jQnGGUBTwovXpjrSK3f5DMhKzYAZ-eOCzbLxEbXS6A_J0oVYkbgbNHcgCXY=w1280" />
+      <meta property="og:title" content="Docutod" />
+      <meta property="og:image" content="https://lh4.googleusercontent.com/qMhAhZstz_q-VSiKxVJmolEpJ4hiG_Nqhx7FlPBlWq2CF6YDLQMx_VLT7iLhjnOtsn50IQcRsO-Y0uc9nXgychOqAlkh7THK4wWNHXqM8jQnGGUBTwovXpjrSK3f5DMhKzYAZ-eOCzbLxEbXS6A_J0oVYkbgbNHcgCXY=w1280" />
     </>
   )
 }
